862e2cd442e69c170adb9500b534afd994c78c33
gorkun <dmitry.gorkun@oodji.com>
2023-12-18 21:05:04 +0100

    comments vk

--- app/Actions/VK/VKRequestHandler.php
+++ app/Actions/VK/VKRequestHandler.php
@@ -4,6 +4,7 @@ namespace App\Actions\VK;
 
 use App\Facades\TechBotFacade;
 use App\Models\Author;
+use App\Models\Channel;
 use App\Models\Message;
 use App\Models\MessageSchedule;
 use App\Models\Place;
@@ -12,6 +13,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 use TelegramBot\Api\BotApi;
 use TelegramBot\Api\HttpException;
 use TelegramBot\Api\Types\ReplyKeyboardMarkup;
@@ -20,14 +22,36 @@ class VKRequestHandler
 {
     public function handle(Request $request)
     {
-        $data = $request->toArray();
-        Log::info('vk push - ' . count($data), $data);
-        Log::info('vk headers - ', $request->headers->all());
+        try {
+            $data = $request->toArray();
+            Log::info('vk push - ' . count($data), $data);
 
-        if($request->get('secret') != config()->get('app.vk_group_secret')) {
-            return;
-        } elseif ($data['type'] == 'wall_reply_new') {
-            TechBotFacade::send("Новый комментарий в группе {$data['group_id']} к посту {$data['object']['post_id']} : {$data['object']['text']}");
+            if($request->get('secret') != config()->get('app.vk_group_secret')) {
+                return;
+            } elseif ($data['type'] == 'wall_reply_new') {
+                $channel = Channel::query()
+                    ->where('tg_id', $data['group_id'])
+                    ->where('type', 'vk')
+                    ->first()
+                ;
+
+                if($channel) {
+                    $channel_link = "https://vk.com/wall-{$data['group_id']}";
+                    $message = (__('webapp.comments.vk', [
+                        'link' => $channel_link . '_' . $data['object']['post_id'],
+                        'text' => Str::of($data['object']['text'])->words(10, '...'),
+                        'channel' => $channel ? $channel->name : 'Unknown group',
+                        'channel_link' => $channel_link,
+                    ]));
+
+                    $botApi = new BotApi($channel->telegram_bot->api_token);
+                    $botApi->sendMessage($channel->telegram_bot->moderation_group, $message, 'HTML');
+                } else {
+                    Log::info('Cannot find channel ' . $data['group_id']);
+                }
+            }
+        } catch (\Exception $exception) {
+            Log::info('vk - push',['error' =>  $exception->getMessage()]);
         }
 
     }
