1a18a5dca37af4a029734d49d62965f3b99a774f
gorkun <dmitry.gorkun@oodji.com>
2023-12-21 13:55:30 +0100

    f

--- app/Console/Commands/SendMessages.php
+++ app/Console/Commands/SendMessages.php
@@ -40,8 +40,8 @@ class SendMessages extends Command
 ;
             foreach ($messageSchedules as $messageSchedule) {
                 foreach ($messageSchedule->channels as $channel) {
-                    $messageSchedule->update(['status' => 'process']);
                     ProcessMessage::dispatch($messageSchedule, $channel)->onQueue($channel->type);
+                    $messageSchedule->update(['status' => 'process']);
                 }
             }
 
