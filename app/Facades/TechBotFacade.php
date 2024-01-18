dbd70942393130f1ebf321c7cc872cf807a84bbe
gorkun <dmitry.gorkun@oodji.com>
2024-01-18 21:37:53 +0100

    web-interface test

--- app/Facades/TechBotFacade.php
+++ app/Facades/TechBotFacade.php
@@ -5,7 +5,7 @@ namespace App\Facades;
 use App\Models\Message;
 
 /**
- * @method static array createMedia(Message $message)
+ * @method static array createMedia(Message $message, $text = false)
  * @method static string send(string $message)
  */
 class TechBotFacade extends \Illuminate\Support\Facades\Facade
