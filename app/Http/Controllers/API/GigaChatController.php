740e7343e492565a9faa4f80a4a647308abe308e
berryllium <gorkund@yandex.ru>
2025-07-22 01:33:45 +0200

    f

--- app/Http/Controllers/API/GigaChatController.php
+++ app/Http/Controllers/API/GigaChatController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Services\GigaChatService;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Throwable;
 
@@ -32,9 +33,10 @@ class GigachatController extends Controller
             if (!$prompt) {
                 throw new Exception('Введите запрос!');
             }
-            $image = $giga->getImage($prompt);
-            return response()->json(['success' => true, 'image' => asset(Storage::url($image)), 'image_path' => $image ]);
+            $image_path = $giga->getImage($prompt);
+            return response()->json(['success' => true, 'image' => asset(Storage::url($image_path)), 'image_path' => $image_path ]);
         } catch (Throwable $e) {
+            Log::error($e->getMessage(), ['trace' => $e->getTraceAsString()]);
             return response()->json(['error' => $e->getMessage()]);
         }
     }
