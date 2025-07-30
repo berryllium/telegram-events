6c4b8d1cc179ba069159f43a362642c9630d3fa7
berryllium <gorkund@yandex.ru>
2025-07-30 12:11:10 +0200

    f

--- resources/views/components/detailed-table.blade.php
+++ resources/views/components/detailed-table.blade.php
@@ -1,6 +1,7 @@
 <div class="test">
     <button role="detail-button" class="btn btn-primary" type="button" style="width: 120px" data-url="{{ $url }}"
         data-token="{{ csrf_token() }}" data-payload="{{ json_encode($payload) }}">Подробнее</button>
-    <script src="{{ asset('asset/js/components/detailed-table.js') }}"></script>
-
+    @once
+        <script src="{{ asset('asset/js/components/detailed-table.js') }}"></script>
+    @endonce
 </div>
